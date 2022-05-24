8afb8807fd02b33a9151c9ae96fe8a63e1ffb290
Mack Gallagher <wingsonthebus@gmail.com>
2022-05-24 01:41:06 -0500

    meet MVP

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,10 +1,50 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Display from './../Display';
 
-test('renders without errors with no props', async () => { });
+const showData = {
+  name: 'ljafsa',
+  summary: 'ajlfsdkjlfksa',
+  seasons: [
+             {
+               id: 0,
+               episodes: [],
+             },
+             {
+               id: 1,
+               episodes: [],
+             },
+             {
+               id: 2,
+               episodes: [],
+             },
+           ],
 
-test('renders Show component when the button is clicked ', () => { });
+}
 
-test('renders show season options matching your data when the button is clicked', () => { });
+test('renders without errors with no props', async () => {
+  render(<Display />);
+});
+
+test('renders Show component when the button is clicked ', async () => {
+  const wrapper = render(<Display />);
+  const button = wrapper.queryByText(/Show Data/i);
+  fireEvent.click(button);
+  await waitFor(() => expect(wrapper.queryByTestId('show-container')).toBeInTheDocument() )
+});
+
+test('renders show season options matching your data when the button is clicked', async () => {
+  const wrapper = render(<Display />);
+  const button = wrapper.queryByText(/Show Data/i);
+  fireEvent.click(button);
+  await waitFor(() => expect(wrapper.queryAllByTestId('season-option')).toHaveLength(4) )
+});
+
+test('callback is called when button is clicked', async () => {
+  const mock = jest.fn();
+  const wrapper = render(<Display displayFunc={mock} />);
+  const button = wrapper.queryByText(/Show Data/i);
+  fireEvent.click(button);
+  await waitFor(() => expect(mock).toHaveBeenCalled());
+});
