2d60cf8c38b929d942da9b85886435d1cdcd66cb
Mack Gallagher <wingsonthebus@gmail.com>
2022-05-23 22:54:53 -0500

    finish tests for Episode component

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,8 +3,58 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Episode from './../Episode';
 
-test("renders without error", () => { });
+const exampleEpisode = {
+  id: "foiafh",
+  image: '',
+  name: 'ajlfhwo',
+  season: [],
+  number: 'lakfoew',
+  summary: 'owf',
+  runtime: 0,
+}
 
-test("renders the summary test passed as prop", () => { });
+const specificSummaryData = {
+  id: "foiafh",
+  image: '',
+  name: 'ajlfhwo',
+  season: [],
+  number: 'lakfoew',
+  summary: 'AAAA',
+  runtime: 0,
+}
 
-test("renders default image when image is not defined", () => { });
+const nullImageData = {
+  id: "foiafh",
+  image: null,
+  name: 'ajlfhwo',
+  season: [],
+  number: 'lakfoew',
+  summary: 'owf',
+  runtime: 0,
+}
+
+test("renders without error", () => {
+  render(<Episode
+           episode={exampleEpisode}
+         />)
+});
+
+test("renders the summary test passed as prop", () => {
+  const episode = render(<Episode
+                           episode={specificSummaryData}
+                         />);
+  const { container } = episode;
+  let summary = container.querySelectorAll('p')[1];
+  expect(summary).toHaveTextContent('AAAA');
+  expect(summary.textContent).toBe('AAAA');
+  summary = episode.queryByText('AAAA');
+  expect(summary).toBeInTheDocument();
+});
+
+test("renders default image when image is not defined", () => {
+  const episode = render(<Episode
+                           episode={nullImageData}
+                         />);
+  const img = episode.queryByAltText('https://i.ibb.co/2FsfXqM/stranger-things.png');
+  expect(img).toBeInTheDocument(); 
+});
